b12ed9e019bf6fc10f344d2d5d1f5a0a7d5fb9a2
ROHIT4321 <rppatil3210@gmail.com>
2017-05-09 15:18:34 +0530

    This is the test Project for the GitHub
    For the branch and pull request.

---
+++ PATH.m
@@ -0,0 +1,10 @@
+clear all
+close all
+clc;
+
+path = 'E:\[01] IMAGE SAMPLES\PETCT_Neck 1HEAD_NECK\CT HEAD-NK 5.0 B30s.dcm';
+
+% TD_Creator(path);
+
+TD_Creator(path);
+
---
+++ random.m
@@ -0,0 +1,8 @@
+function Y = random()
+clear all
+clc
+close all
+
+A = randi(10,5)
+
+end
